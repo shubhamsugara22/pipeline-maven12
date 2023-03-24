667c6e463c570e3610966a8438e2f3803c48aa91
shubhamsugara22 <shubhamsugara22@gmail.com>
2023-03-25 01:35:48 +0530

    correct script for build and test

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "**************************************"
 
 WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm  -ti -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:latest  "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:latest  "$@"
