6220e7c8701d01c19ae0a9c2851d5eafd7c1439c
shubhamsugara22 <shubhamsugara22@gmail.com>
2023-03-24 23:57:27 +0530

    Fixed workspace path for job exectuion

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,6 @@ echo "**************************************"
 echo "********Testing the Code******************"
 echo "**************************************"
 
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm  -ti -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:latest  "$@"
+docker run --rm  -ti -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:latest  "$@"
